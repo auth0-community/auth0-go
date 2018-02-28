47add48c9f62f4ecb116dc69836b439d79e86f0c
Max Ekman <max@looplab.se>
2018-02-28 18:54:10 +0100

    Add FromParams token extractor

--- token_extraction.go
+++ token_extraction.go
@@ -1,17 +1,17 @@
 package auth0
 
 import (
-
-	"net/http"
-	"gopkg.in/square/go-jose.v2/jwt"
-	"strings"
 	"errors"
+	"net/http"
+	"strings"
 
+	"gopkg.in/square/go-jose.v2/jwt"
 )
 
 var (
 	ErrTokenNotFound = errors.New("Token not found")
 )
+
 // RequestTokenExtractor can extract a JWT
 // from a request.
 type RequestTokenExtractor interface {
@@ -42,6 +42,22 @@ func FromHeader(r *http.Request) (*jwt.JSONWebToken, error) {
 func fromHeader(r *http.Request) ([]byte, error) {
 	if authorizationHeader := r.Header.Get("Authorization"); len(authorizationHeader) > 7 && strings.EqualFold(authorizationHeader[0:7], "BEARER ") {
 		return []byte(authorizationHeader[7:]), nil
+	}
+	return nil, ErrTokenNotFound
+}
+
+// FromParams returns the JWT when passed as the URL query param "token".
+func FromParams(r *http.Request) (*jwt.JSONWebToken, error) {
+	raw, err := fromParams(r)
+	if err != nil {
+		return nil, err
+	}
+	return jwt.ParseSigned(string(raw))
+}
+
+func fromParams(r *http.Request) ([]byte, error) {
+	if token := r.URL.Query().Get("token"); token != "" {
+		return []byte(token), nil
 	}
 	return nil, ErrTokenNotFound
 }
--- token_extraction_test.go
+++ token_extraction_test.go
@@ -8,19 +8,40 @@ import (
 	"time"
 
 	"gopkg.in/square/go-jose.v2"
-
 	"gopkg.in/square/go-jose.v2/jwt"
 )
 
-func TestFromRequestExtraction(t *testing.T) {
+func TestFromRequestHeaderExtraction(t *testing.T) {
 	referenceToken := getTestToken(defaultAudience, defaultIssuer, time.Now(), jose.HS256, defaultSecret)
-	headerTokenRequest, _ := http.NewRequest("", "http://localhost", nil)
 
+	headerTokenRequest, _ := http.NewRequest("", "http://localhost", nil)
 	headerValue := fmt.Sprintf("Bearer %s", referenceToken)
 	headerTokenRequest.Header.Add("Authorization", headerValue)
 
 	token, err := FromHeader(headerTokenRequest)
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
+	claims := jwt.Claims{}
+	err = token.Claims([]byte("secret"), &claims)
+	if err != nil {
+		t.Errorf("Claims should be decoded correctly with default token: %q \n", err)
+		t.FailNow()
+	}
+
+	if claims.Issuer != defaultIssuer || !reflect.DeepEqual(claims.Audience, jwt.Audience(defaultAudience)) {
+		t.Error("Invalid issuer, audience or subject:", claims.Issuer, claims.Audience)
+	}
+}
+
+func TestFromRequestParamsExtraction(t *testing.T) {
+	referenceToken := getTestToken(defaultAudience, defaultIssuer, time.Now(), jose.HS256, defaultSecret)
+
+	paramTokenRequest, _ := http.NewRequest("", "http://localhost?token="+referenceToken, nil)
+
+	token, err := FromParams(paramTokenRequest)
 	if err != nil {
 		t.Error(err)
 		return
